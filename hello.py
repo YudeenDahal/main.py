3949d3bac8aef7c8f78e299a852e74d4b8ff965f
Yudeen Dahal <yudeendahal80@gmail.com>
2025-10-02 07:56:12 +0545

    arthimetic operators

--- hello.py
+++ hello.py
@@ -1,9 +1,43 @@
-print("hello world"," my age is 23") 
-print("nothing much")
-print (45+5)
-print("name = shradha")
-age = "24"
-price = "25.99"
-print(age, price)
-A = "19"
-print(A)
+# # # """ print("hello world"," my age is 23") 
+# # # print("nothing much")
+# # # print (45+5)
+# # # print("name = shradha")
+# # # age = "24"
+
+# # # book = age
+
+# # # name = ram 
+
+
+
+# # # price = 25.99
+# # # print(age, price)
+# # # A = "19"
+# # # print(A)
+# # # print("my name is: ", age)
+
+# # # print(type(name))
+# # # print(type(age))
+# # # print(type(price))  """
+
+# # age = 23
+# # old  = false 
+# # a = none
+# # print(type(old))
+# # print(type(a))
+
+# # #print("nothing")
+
+a = 6
+b = 6
+
+# print(a+b)
+# print(a-b)
+# print(a*b)
+# print(a/b)
+# print(a%b)
+# print(a**b)
+
+
+print(a == b)
+print(a != b)

924a42dfc93f13e8c5115f41ab7bc5af630888bb
Yudeen Dahal <yudeendahal80@gmail.com>
2025-10-03 14:30:36 +0545

    day 4

--- lecture.py
+++ lecture.py
@@ -1,236 +1,309 @@
-#  # """ print("hello world"," my age is 23") 
-# # # # # # print("nothing much")
-# # # # # # print (45+5)
-# # # # # # print("name = shradha")
-# # # # # # age = "24"
+# #  # """ print("hello world"," my age is 23") 
+# # # # # # # print("nothing much")
+# # # # # # # print (45+5)
+# # # # # # # print("name = shradha")
+# # # # # # # age = "24"
 
-# # # # # # book = age
+# # # # # # # book = age
 
-# # # # # # name = ram 
-
-
-
-# # # # # # price = 25.99
-# # # # # # print(age, price)
-# # # # # # A = "19"
-# # # # # # print(A)
-# # # # # # print("my name is: ", age)
-
-# # # # # # print(type(name))
-# # # # # # print(type(age))
-# # # # # # print(type(price))  """
-
-# # # # # age = 23
-# # # # # old  = false 
-# # # # # a = none
-# # # # # print(type(old))
-# # # # # print(type(a))
-
-# # # # # #print("nothing")
-
-# # a = 6
-# # b = 6
-
-# # # # print(a+b)
-# # # # print(a-b)
-# # # # print(a*b)
-# # # # print(a/b)
-# # # # print(a%b)
-# # # # print(a**b)
-
-
-# # # print(a == b)
-# # # print(a != b)
-# # # print(a > b)
-# # # print(a < b)
-# # # print(a == b)
-# # # print(a != b)
-
-# # # num = 10
-# # # num = 10 + num
-# # # print("num is : ", num)
+# # # # # # # name = ram 
 
 
 
+# # # # # # # price = 25.99
+# # # # # # # print(age, price)
+# # # # # # # A = "19"
+# # # # # # # print(A)
+# # # # # # # print("my name is: ", age)
 
+# # # # # # # print(type(name))
+# # # # # # # print(type(age))
+# # # # # # # print(type(price))  """
+
+# # # # # # age = 23
+# # # # # # old  = false 
+# # # # # # a = none
+# # # # # # print(type(old))
+# # # # # # print(type(a))
+
+# # # # # # #print("nothing")
+
+# # # a = 6
+# # # b = 6
+
+# # # # # print(a+b)
+# # # # # print(a-b)
+# # # # # print(a*b)
+# # # # # print(a/b)
+# # # # # print(a%b)
+# # # # # print(a**b)
+
+
+# # # # print(a == b)
+# # # # print(a != b)
+# # # # print(a > b)
+# # # # print(a < b)
+# # # # print(a == b)
+# # # # print(a != b)
+
+# # # # num = 10
+# # # # num = 10 + num
+# # # # print("num is : ", num)
+
+
+
+
+# # # # print(not (a>b))
+
+# # # print(not False)
 # # # print(not (a>b))
 
-# # print(not False)
-# # print(not (a>b))
+# # # val1 = False
+# # # val2 = False
+# # # print("AND operator:", val1 or val2)
 
-# # val1 = False
-# # val2 = False
-# # print("AND operator:", val1 or val2)
-
-# # print("OR operator:", val1 or val2)
+# # # print("OR operator:", val1 or val2)
 
 
-# a, b = 1,2.1
-# sum = a + b
-# print (sum)
+# # a, b = 1,2.1
+# # sum = a + b
+# # print (sum)
 
-# name = input ("enter you name :")
-# print ("welcome,", name)
+# # name = input ("enter you name :")
+# # print ("welcome,", name)
 
-# val =  int(input("enter some value :"))
-# print(type(val), val)
-
-
-# name = input("enter your name :")
-# print ("welcome,", name)
-# marks = input("enter your marks in physics :")
-# print ("Your grade is,", marks)
-# address = input("enter your addres :")
-# print ("Your address is,,", address)
+# # val =  int(input("enter some value :"))
+# # print(type(val), val)
 
 
-# print ("your biodata is,", name, marks, address )
+# # name = input("enter your name :")
+# # print ("welcome,", name)
+# # marks = input("enter your marks in physics :")
+# # print ("Your grade is,", marks)
+# # address = input("enter your addres :")
+# # print ("Your address is,,", address)
 
-# practice session
+
+# # print ("your biodata is,", name, marks, address )
+
+# # practice session
 
 
-# first = int(input("Enter any first number : "))
-# second = int(input("Enter any second number : "))
+# # first = int(input("Enter any first number : "))
+# # second = int(input("Enter any second number : "))
 
-# print("sum =", first + second)
+# # print("sum =", first + second)
 
-#practice session
-# A = float(input("side1 ="))
-# B = float(input("side2 ="))
-# print("avg is,", (A+B)/2)
+# #practice session
+# # A = float(input("side1 ="))
+# # B = float(input("side2 ="))
+# # print("avg is,", (A+B)/2)
 
-# practice session
+# # practice session
 
-# A = int(input("enter first number"))
-# B = int(input("enter second number"))
-# print (A <= B)
+# # A = int(input("enter first number"))
+# # B = int(input("enter second number"))
+# # print (A <= B)
 
 
 
-# # val1 = False
-# # val2 = False
-# # print("AND operator:", val1 or val2)
+# # # val1 = False
+# # # val2 = False
+# # # print("AND operator:", val1 or val2)
 
-# # print("OR operator:", val1 or val2)
-
-
-
-# 2nd video
-# # Strings 
-
-# str1 = "this is a stringThis is another string "
-# str2 = "Apna collage"
-# str = "nothing"
-# print(str1+str2 )
-
-
-# (len(str))
-# print (len(str))
-
-# indexing - is the way of giving no. to every alphabate 
-
-# str = "Rajan Dahal"
-# print(str[3])
-
-# slicing
-
-# str = "apna collage $$"
-# print(str[1:7])
-# print (str[0:len(str)])
-# print(str[-5:])
-
-# print(str.endswith("age"))
-# str = str.capitalize()
-# print(str)
-
-# print(str.replace("o", "a"))
-
-# print(str.find("o"))
-# print(str.count("o"))
-
-# name = input("enter your name : ")
-# print("length of your name is ", len(name))
-
-# print(str.count("$"))
-
-# light = "red "
-# if (light == "red"):
-#     print("stop")
-#     elif(light == "green"):
-#         print("go")
-#         elif(light == "yekkiw"):
-#             print("look")
-#     print("end of code")
-# light = "red"
-
-# if light == "red":
-#     print("stop")
-# elif light == "green":
-#     print("go")
-# elif light == "yellow":
-
-#     print("look")
-
-# print("end of code")
-
-# num = 5 
-
-# if num > 4:
-#     print("number is greater")
-# elif num < 2:
-#     print("number is not greater")
-# else:
-#     print("number is between 2 and 8")
-
-# print("the code ended")
+# # # print("OR operator:", val1 or val2)
 
 
 
-# num = 5
+# # 2nd video
+# # # Strings 
 
-# if (num == 5):
-#     print("num is greater")
-# else:
-#     print("number is nto greater") 
-
-
-# marks = int(input("enter your grade, "))
-
-# if(marks >= 90) :
-#     print("You're grade is A")
-# elif(marks >= 80) and marks < 90 :
-#     print("You're grade is B")
-# elif(marks >= 70) and marks < 80 :
-#     print("You're grade is C")
-# else :
-#     print("You're grade is D")
-
-# age = int(input("Your age is, "))
-# if (age >= 18) :
-#     if (age >= 80) :
-#         print("cannot drive")
-#     else:
-#         print("can drive ")
-
-# num = int(input("give any number : "))
-# if(num % 2 == 0):
-#     print("your number is even", num)
-# else :
-#     print ("your number is odd")
+# # str1 = "this is a stringThis is another string "
+# # str2 = "Apna collage"
+# # str = "nothing"
+# # print(str1+str2 )
 
 
-# num1 = int(input("Give any first number, "))
-# num2 = int(input("Give any second number, "))
-# num3 = int(input("Give any third number, "))
-# if (num1 > num2 and num1 > num3 ):
-#     print("First number is greater", num1)
-# if (num2 > num1 and num2 > num3 ):
-#     print("second number is greater", num2)
-# if (num3 > num2 and num3 > num1 ):
-    # print("third number is greater", num3)
+# # (len(str))
+# # print (len(str))
 
-# num = int(input("give any number"))
-# if (num % 7 == 0):
-#     print("number is multiple of 7")
-# else:
-#     print("number is not multiple of 7")
+# # indexing - is the way of giving no. to every alphabate 
+
+# # str = "Rajan Dahal"
+# # print(str[3])
+
+# # slicing
+
+# # str = "apna collage $$"
+# # print(str[1:7])
+# # print (str[0:len(str)])
+# # print(str[-5:])
+
+# # print(str.endswith("age"))
+# # str = str.capitalize()
+# # print(str)
+
+# # print(str.replace("o", "a"))
+
+# # print(str.find("o"))
+# # print(str.count("o"))
+
+# # name = input("enter your name : ")
+# # print("length of your name is ", len(name))
+
+# # print(str.count("$"))
+
+# # light = "red "
+# # if (light == "red"):
+# #     print("stop")
+# #     elif(light == "green"):
+# #         print("go")
+# #         elif(light == "yekkiw"):
+# #             print("look")
+# #     print("end of code")
+# # light = "red"
+
+# # if light == "red":
+# #     print("stop")
+# # elif light == "green":
+# #     print("go")
+# # elif light == "yellow":
+
+# #     print("look")
+
+# # print("end of code")
+
+# # num = 5 
+
+# # if num > 4:
+# #     print("number is greater")
+# # elif num < 2:
+# #     print("number is not greater")
+# # else:
+# #     print("number is between 2 and 8")
+
+# # print("the code ended")
+
+
+
+# # num = 5
+
+# # if (num == 5):
+# #     print("num is greater")
+# # else:
+# #     print("number is nto greater") 
+
+
+# # marks = int(input("enter your grade, "))
+
+# # if(marks >= 90) :
+# #     print("You're grade is A")
+# # elif(marks >= 80) and marks < 90 :
+# #     print("You're grade is B")
+# # elif(marks >= 70) and marks < 80 :
+# #     print("You're grade is C")
+# # else :
+# #     print("You're grade is D")
+
+# # age = int(input("Your age is, "))
+# # if (age >= 18) :
+# #     if (age >= 80) :
+# #         print("cannot drive")
+# #     else:
+# #         print("can drive ")
+
+# # num = int(input("give any number : "))
+# # if(num % 2 == 0):
+# #     print("your number is even", num)
+# # else :
+# #     print ("your number is odd")
+
+
+# # num1 = int(input("Give any first number, "))
+# # num2 = int(input("Give any second number, "))
+# # num3 = int(input("Give any third number, "))
+# # if (num1 > num2 and num1 > num3 ):
+# #     print("First number is greater", num1)
+# # if (num2 > num1 and num2 > num3 ):
+# #     print("second number is greater", num2)
+# # if (num3 > num2 and num3 > num1 ):
+#     # print("third number is greater", num3)
+
+# # num = int(input("give any number"))
+# # if (num % 7 == 0):
+# #     print("number is multiple of 7")
+# # else:
+# # #     print("number is not multiple of 7")
+
+# # mark = [90, 40, 60, 670, 28]
+# # print(mark)
+# # print(type(mark))
+# # print(mark[0])
+# # print(len(mark))
+
+
+# # student = ["karan", "itahari-4"]
+# # print(student[0])
+# # student[0] = "Ram"
+# # print(student)
+
+# # list = [1, 5, 9]
+# # list.append(4)
+# # print(list)
+        
+# # list.sort(reverse=True)
+# # list.sort()
+# # print(list)
+
+
+# # list.insert(3,5)
+# # print(list)
+
+
+# # list.remove(5)
+
+# # list.pop(3)
+
+# # tup = (80,34, 69)
+# # print(tup.count(80))
+
+# # movies = []
+
+# # mov1 = (input("enter your top 1 fav movie: "))
+# # mov2 = (input("enter your top 2 fav movie: "))
+# # mov3 = (input("enter your top 3 fav movie: "))
+
+# # movies.append(mov1)
+# # movies.append(mov2)
+# # movies.append(mov3)
+
+# # print(movies)
+
+
+# # list = [1,2,3,2,1]
+
+# # if(list.sort(reverse=True) == list.sort()):
+
+# #     print("list is palindrome")
+
+# # else:
+# #     print("list is not palindrome")
+
+
+# # nums = [1, 2, 3, 2, 2]
+
+# # if nums == nums[::-1]:   # compare list with its reverse
+# #     print("list is palindrome")
+
+# # else:
+
+# #     print("list is not palindrome")
+
+
+# list = ["c", "d", "a", "a", "b", "b", "a"]
+# # tup.count("a")
+# # print(tup.count("a"))
+# list.sort()
+# print(list)
+
+
